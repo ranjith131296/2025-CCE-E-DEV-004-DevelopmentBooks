1b495f88682f5a524d5657b28f4fbe36ee8a0849
VN999RA <ranjithraj.n@pearson.com>
2025-07-24 12:08:23 +0530

    LeapYear Test Class Creation

---
+++ src/main/java/com/bnpparibas/code/test/LeapYear.java
@@ -0,0 +1,14 @@
+package com.bnpparibas.code.test;
+
+public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        if (year % 100 == 0) {
+            return false;
+        }
+        return year % 4 == 0;
+    }
+}
