c4564fb71d18900accabc157baf30a4b2561e271
VN999RA <ranjithraj.n@pearson.com>
2025-07-24 12:10:47 +0530

    LeapYear Test Class Creation

--- src/test/java/com/bnpparibas/code/test/LeapYearTests.java
+++ src/test/java/com/bnpparibas/code/test/LeapYearTests.java
@@ -1,4 +1,17 @@
 package com.bnpparibas.code.test;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LeapYearTests {
+
+
+    @Test
+    public void isLeapYears() {
+        assertTrue(LeapYear.isLeapYear(1600));
+        assertTrue(LeapYear.isLeapYear(2000));
+        assertTrue(LeapYear.isLeapYear(2400));
+    }
+
 }
