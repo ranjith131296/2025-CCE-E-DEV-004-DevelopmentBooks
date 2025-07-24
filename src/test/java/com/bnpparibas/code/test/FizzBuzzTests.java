0c933cdf2e70a932ffd4696559f9d77897809c79
VN999RA <ranjithraj.n@pearson.com>
2025-07-24 11:53:07 +0530

    FizzBuzz Test Class Creation

---
+++ src/test/java/com/bnpparibas/code/test/FizzBuzzTests.java
@@ -0,0 +1,19 @@
+package com.bnpparibas.code.test;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTests {
+
+    @Test
+    public void testFizzBuzz() {
+
+    }
+
+    @Test
+    public void testFizz() {
+    }
+
+    @Test
+    public void testBuzz() {
+    }
+}
